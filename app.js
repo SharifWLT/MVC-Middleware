6afb83933a6dc24557f5e7d684646d9f4ae96394
Fingolfin01 <42465549+Fingolfin01@users.noreply.github.com>
2019-07-11 14:28:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var convert = require('color-convert');
 var fs = require("fs");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+<<<<<<< HEAD
+const port = 3000
+=======
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+>>>>>>> 5b86171380b04a7c9934b59298bb5d3c5d2a2c0d
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Use Express.router to group your two routes in one "/color" router
+// What happens when the user did not define a query.color parameter, 
+    // your code should not break
+
+app.get('/color/1', function(req,res){
+  const {query} = req
+  res.json({ message: "Color 1 is" + query.color})
+})
+
+app.get("/color/2", function(req,res){
+  const {query} = req
+  res.json({ message: "Color 2 is" + query.color})
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,6 +50,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+<<<<<<< HEAD
+app.listen(port, () => console.log("Server started on port: " + port))
+=======
  // MyLogger
 app.use(
   function(req, res, next) {
@@ -55,3 +67,4 @@ app.use(
 
 
 module.exports = app;
+>>>>>>> 5b86171380b04a7c9934b59298bb5d3c5d2a2c0d
